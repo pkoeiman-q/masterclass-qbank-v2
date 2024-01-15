c6a83beed0c67474ec1b93c0d5b87d699ba6e870
Pascal Koeiman <pkoeiman@quintor.nl>
2024-01-15 13:28:41 +0100

    Add endpoint to test whether authentication works

---
+++ MasterclassApiTest/Controllers/GebruikersController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using MasterclassApiTest.Models;
+
+namespace MasterclassApiTest.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class GebruikersController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/[controller]/public")]
+        [AllowAnonymous]
+        public IActionResult Public()
+        {
+            return Ok("Hoi, dit is een publieke endpoint");
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/admin")]
+        public IActionResult Admin()
+        {
+            var huidigeGebruiker = GetCurrentUser();
+            return Ok($"Hoi {huidigeGebruiker.GebruikersNaam}, je rol is {huidigeGebruiker.Role}");
+        }
+
+        private Gebruiker GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                return new Gebruiker
+                {
+                    GebruikersNaam = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value,
+                    Email = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
+                    Role = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value
+                };
+            }
+            return null;
+        }
+    }
+}
