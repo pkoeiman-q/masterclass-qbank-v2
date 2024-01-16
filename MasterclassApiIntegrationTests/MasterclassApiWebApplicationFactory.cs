dc14e23512bbb80cca017e69ffa926df6627f224
Pascal Koeiman <pkoeiman@quintor.nl>
2024-01-16 12:42:40 +0100

    Add integration tests

---
+++ MasterclassApiIntegrationTests/MasterclassApiWebApplicationFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace MasterclassApiIntegrationTests
+{
+    // Integration test tutorial: https://www.youtube.com/watch?v=RXSPCIrrjHc
+    internal class MasterclassApiWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                // Remove the normal connection string/database options
+                // Replace it with different settings for testing purposes
+            });
+        }
+    }
+}
