afcd182793a9c2d6dc6b4fed0314074fccc03547
Pascal Koeiman <pkoeiman@quintor.nl>
2024-01-17 15:25:52 +0100

    Add KlantInput class, used to take in user data for creating/updating a Klant

---
+++ MasterclassApiTest/Models/KlantInput.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MasterclassApiTest.Models
+{
+    public class KlantInput
+    {
+        [StringLength(50, MinimumLength = 3)]
+        public string LoginNaam { get; set; }
+        public string Voorletters { get; set; }
+        public string Achternaam { get; set; }
+        public string Geslacht { get; set; }
+        public DateTime GeboorteDatum { get; set; }
+        public DateTime? OverlijdensDatum { get; set; }
+        public string Straat { get; set; }
+        public int Huisnummer { get; set; }
+        public string? HuisnummerToevoeging { get; set; }
+        public string Postcode { get; set; }
+        public string Woonplaats { get; set; }
+        public int Bsn { get; set; }
+        public string TelefoonNummer { get; set; }
+        public string Email { get; set; }
+    }
+}
