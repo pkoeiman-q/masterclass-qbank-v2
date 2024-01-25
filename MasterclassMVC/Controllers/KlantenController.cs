cbb933a222d42911353c3cdf3fe21565adae6036
Pascal Koeiman <pkoeiman@quintor.nl>
2024-01-25 11:58:24 +0100

    Require ASP.NET Identity auth when using MVC klantencontroller. It sends a JWT token to the API when making requests from there

--- MasterclassMVC/Controllers/KlantenController.cs
+++ MasterclassMVC/Controllers/KlantenController.cs
@@ -2,28 +2,33 @@
 using MasterclassApiTest.Entities;
 using MasterclassApiTest.Models;
 using MasterclassApiTest.Pagination;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
 namespace MasterclassMVC.Controllers
 {
+    [Authorize]
     public class KlantenController : Controller
     {
-        private Uri _baseAdress = new Uri("https://localhost:7267/api/klanten");
+        private Uri _klantUri = new Uri("https://localhost:7267/api/klanten");
         private HttpClient _apiClient;
+        private readonly IConfiguration _config;
 
-        public KlantenController()
+        public KlantenController(IConfiguration config)
         {
+            _config = config;
             _apiClient = new HttpClient();
-            _apiClient.BaseAddress = _baseAdress;
-            _apiClient.DefaultRequestHeaders.Add("X-Api-Version", "2.0");
+            _apiClient.DefaultRequestHeaders.Add("Accept", "application/json;x-api-version=2.0");
+            _apiClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {_config.GetValue<string>("ApiJwtToken")}");
         }
 
         public async Task<IActionResult> Index()
         {
-            var klanten = await _apiClient.GetFromJsonAsync<List<GetKlantDTO>>(_baseAdress);
+            var klanten = await _apiClient.GetFromJsonAsync<List<GetKlantDTO>>(_klantUri);
             return View(klanten);
         }
 
@@ -36,35 +41,30 @@ namespace MasterclassMVC.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CreateKlantDTO createKlantDTO)
         {
-            var response = await _apiClient.PostAsJsonAsync(_baseAdress, createKlantDTO);
+            var response = await _apiClient.PostAsJsonAsync(_klantUri, createKlantDTO);
             if (response.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
             }
 
             return View("Error");
-            // var body = await response.Content.ReadAsStringAsync();
-            // GetKlantDTO? getKlantDTO = JsonConvert.DeserializeObject<GetKlantDTO>(body);
         }
 
         [HttpGet]
         [Route("/klanten/update/{klantId}")]
+        // TODO: fix it so the update page can be loaded even if the Redis cache is down
         public async Task<IActionResult> Update(int klantId)
         {
-            GetKlantDTO? klant = await _apiClient.GetFromJsonAsync<GetKlantDTO>($"{_baseAdress}/{klantId}");
+            GetKlantDTO? klant = await _apiClient.GetFromJsonAsync<GetKlantDTO>($"{_klantUri}/{klantId}");
             if (klant == null) throw new NullReferenceException();
             return View(klant);
         }
-
-        // TODO: View data aan de view meegeven zodat direct vanuit de form naar de API data verzonden wordt
-        // Vraag hierbij: hoe werkt dat als je authenticatie wil toevoegen?
-        //ViewData["FormActionUrl"] = _baseAdress;
 
         [HttpPost]
         public async Task<IActionResult> Update(GetKlantDTO getKlantDTO)
         {
             var klantId = getKlantDTO.Id;
-            var response = await _apiClient.PutAsJsonAsync($"{_baseAdress}/{klantId}/mvc", getKlantDTO);
+            var response = await _apiClient.PutAsJsonAsync($"{_klantUri}/{klantId}/mvc", getKlantDTO);
             var body = await response.Content.ReadAsStringAsync();
             if (response.IsSuccessStatusCode)
             {
@@ -73,5 +73,11 @@ namespace MasterclassMVC.Controllers
 
             return View("Error");
         }
+
+        /*
+            var request = await _apiClient.GetAsync(_klantUri);
+            var body = request.Content.ReadAsStringAsync();
+            var message = JsonConvert.DeserializeObject(body.Result);
+        */
     }
 }
