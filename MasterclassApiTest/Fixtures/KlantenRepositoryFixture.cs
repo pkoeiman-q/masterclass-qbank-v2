c707bd52e13a4e2a1bb13506e7a81f3e8ee5e755
Pascal Koeiman <pkoeiman@quintor.nl>
2024-02-03 14:42:48 +0100

    Add KlantenRepositoryFixture to more easily conduct tests with KlantenRepository

---
+++ MasterclassApiTest/Fixtures/KlantenRepositoryFixture.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using MasterclassApiTest.AutoMapperProfiles;
+using MasterclassApiTest.Builders;
+using MasterclassApiTest.Data;
+using MasterclassApiTest.Entities;
+using MasterclassApiTest.Repositories;
+
+namespace MasterclassApiTest.Fixtures
+{
+    // https://academy.quintor.nl/moodle/mod/lesson/view.php?id=1520&pageid=1339&startlastseen=no
+    // Timestamp 5:45
+    public class KlantenRepositoryFixture
+    {
+        private readonly DataContext _context;
+        public DataContext Context => _context;
+
+        public KlantenRepository Sut { get; }
+
+        public KlantenRepositoryFixture()
+        {
+            _context = new DataContextBuilder()
+                .UseSqlite()
+                .Build();
+
+            // About injecting automapper for tests: https://kenbonny.net/injecting-automapper-profiles-in-tests 
+            var profile = new AutoMappingProfiles();
+            var configuration = new MapperConfiguration(config => config.AddProfile(profile));
+            var mapper = new Mapper(configuration);
+
+            Sut = new KlantenRepository(_context, mapper);
+        }
+
+        public KlantenRepositoryFixture WithKlant(Klant klant)
+        {
+            _context.Klanten.Add(klant);
+            _context.SaveChanges();
+            return this;
+        }
+
+        public KlantenRepositoryFixture WithKlanten(List<Klant> klanten)
+        {
+            foreach (var klant in klanten)
+            {
+                _context.Klanten.Add(klant);
+            }
+            _context.SaveChanges();
+            return this;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
