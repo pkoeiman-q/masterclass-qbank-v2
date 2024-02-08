2dc21dae6e1ca6b6d2dc51c4f92ab865cbba46c0
Pascal Koeiman <pkoeiman@quintor.nl>
2024-02-08 11:02:08 +0100

    RabbitMQ tutorial: add NewTask and Worker classes to allow parallelization of tasks

---
+++ NewTask/NewTask.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+// Relevant info: https://www.rabbitmq.com/tutorials/tutorial-one-dotnet.html
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// Queue declaration is idempotent (it won't be made if it already exists)
+string queue_name = "task_queue";
+channel.QueueDeclare(queue: queue_name,
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var message = GetMessage(args);
+var body = Encoding.UTF8.GetBytes(message);
+var properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
+channel.BasicPublish(exchange: string.Empty,
+    routingKey: queue_name,
+    basicProperties: null,
+    body: body);
+
+Console.WriteLine($" [x] Sent {message}");
+Console.WriteLine("Press [enter] to exit.");
+Console.ReadLine();
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+}
---
+++ Worker/Worker.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// Declare the queue here as well, in case this class instantiates before the Send class
+string queue_name = "task_queue";
+channel.QueueDeclare(queue: queue_name,
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+
+    int dots = message.Split('.').Length - 1;
+    Thread.Sleep(dots * 1000);
+
+    Console.WriteLine(" [x] Done");
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+channel.BasicConsume(queue: queue_name,
+    autoAck: false,
+    consumer: consumer);
+
+Console.WriteLine("Press [Enter] to exit");
+Console.ReadLine();
