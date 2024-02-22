9e9fa71697309c60eb84d651319b56778ebf6366
Pascal Koeiman <pkoeiman@quintor.nl>
2024-02-22 10:51:01 +0100

    Add footer and home component

--- MasterclassAngular2/qbank-angular/src/app/app.component.ts
+++ MasterclassAngular2/qbank-angular/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
